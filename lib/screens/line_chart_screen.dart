4c322959fe51601aab3cdb240d8d80e6ffbec7f0
Alex <awangardowie@gmail.com>
2020-10-30 18:04:54 +0100

    line chart

---
+++ lib/screens/line_chart_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:october_2020_practise_10/custom_widgets/custom_line_chart.dart';
+
+class LineChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+        color: const Color(0xff020227),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: CustomLineChart(),
+        ),
+      );
+}
